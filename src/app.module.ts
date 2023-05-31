06d012973df71517439b27369fe4784e0242e6d4
sdjeon <sdjeon_3top@gsretail.com>
2023-05-31 09:03:07 +0900

    sec1.9 configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// const getEnv = async () => {
+//   return {
+//     PORT: 3000,
+//   };
+// };
+
 @Module({
-  imports: [],
+  // imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
