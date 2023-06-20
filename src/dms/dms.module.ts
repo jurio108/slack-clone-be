8a1e0d7da20256439a28681f9d096dc784e6bdb6
sdjeon <sdjeon_3top@gsretail.com>
2023-06-20 11:18:16 +0900

    add api - dms

--- src/dms/dms.module.ts
+++ src/dms/dms.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DmsService } from './dms.service';
 import { DmsController } from './dms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Workspaces } from 'src/entities/Workspaces';
+import { DMs } from 'src/entities/DMs';
+import { Users } from 'src/entities/Users';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Workspaces, DMs, Users]), EventsModule],
   providers: [DmsService],
   controllers: [DmsController],
 })
